2fa5db362a9c1969afce3a873d96492860502063
R. Miles McCain <x1-key@sendmiles.email>
2020-04-14 17:54:51 -0400

    Improve script resiliency

--- shynet/analytics/templates/analytics/scripts/page.js
+++ shynet/analytics/templates/analytics/scripts/page.js
@@ -3,18 +3,25 @@ window.onload = function () {
     Math.random().toString(36).substring(2, 15) +
     Math.random().toString(36).substring(2, 15);
   function sendUpdate() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", self.location.protocol + "//{{request.site.domain|default:request.META.HTTP_HOST}}{{endpoint}}", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(
-      JSON.stringify({
-        idempotency: idempotency,
-        referrer: document.referrer,
-        loadTime:
-          window.performance.timing.domContentLoadedEventEnd -
-          window.performance.timing.navigationStart,
-      })
-    );
+    try {
+      var xhr = new XMLHttpRequest();
+      xhr.open(
+        "POST",
+        self.location.protocol +
+          "//{{request.site.domain|default:request.META.HTTP_HOST}}{{endpoint}}",
+        true
+      );
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.send(
+        JSON.stringify({
+          idempotency: idempotency,
+          referrer: document.referrer,
+          loadTime:
+            window.performance.timing.domContentLoadedEventEnd -
+            window.performance.timing.navigationStart,
+        })
+      );
+    } catch {}
   }
   setInterval(sendUpdate, 5000);
   sendUpdate();
