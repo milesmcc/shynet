dd6a9d1eaf331e6fb762a717a25144ef06359fea
R. Miles McCain <x1.key.2020@sendmiles.email>
2020-04-28 10:19:05 -0400

    Bump version

--- shynet/shynet/settings.py
+++ shynet/shynet/settings.py
@@ -14,7 +14,7 @@ import os
 from django.contrib.messages import constants as messages
 
 # Increment on new releases
-VERSION = "v0.2.1"
+VERSION = "v0.2.2"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
