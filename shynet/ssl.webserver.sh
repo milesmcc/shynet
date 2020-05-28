1bb4aac32f511fdc3ca09743b6cebb19ca49e9e2
R. Miles McCain <development-machine@sendmiles.email>
2020-05-28 21:40:43 +0000

    Use $PORT env variable

--- shynet/ssl.webserver.sh
+++ shynet/ssl.webserver.sh
@@ -3,7 +3,7 @@
 # Start Gunicorn processes
 echo Launching Shynet web server...
 exec gunicorn shynet.wsgi:application \
-    --bind 0.0.0.0:8080 \
+    --bind 0.0.0.0:${PORT:-8080} \
     --workers ${NUM_WORKERS:-1} \
     --timeout 100 \
     --certfile=cert.pem \
--- shynet/webserver.sh
+++ shynet/webserver.sh
@@ -2,6 +2,6 @@
 # Start Gunicorn processes
 echo Launching Shynet web server...
 exec gunicorn shynet.wsgi:application \
-    --bind 0.0.0.0:8080 \
+    --bind 0.0.0.0:${PORT:-8080} \
     --workers ${NUM_WORKERS:-1} \
     --timeout 100
