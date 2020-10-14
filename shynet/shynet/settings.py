a8fd263855380f703895e0316f1b5e7320c79df6
R. Miles McCain <development-machine@sendmiles.email>
2020-10-14 18:00:35 +0000

    Bump version

--- shynet/shynet/settings.py
+++ shynet/shynet/settings.py
@@ -18,7 +18,7 @@ import urllib.parse as urlparse
 from django.contrib.messages import constants as messages
 
 # Increment on new releases
-VERSION = "v0.6.5"
+VERSION = "v0.6.6"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
