8bab14cc8abba63f3559718ce8847c7bef423c8a
R. Miles McCain <github@sendmiles.email>
2021-03-29 15:04:36 +0000

    Separate bounce migration into two

--- shynet/shynet/settings.py
+++ shynet/shynet/settings.py
@@ -18,7 +18,7 @@ import urllib.parse as urlparse
 from django.contrib.messages import constants as messages
 
 # Increment on new releases
-VERSION = "v0.8.1"
+VERSION = "v0.8.2alpha"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
