68945df17dc0e89449c6a79b00ad84f0a9e35f78
R. Miles McCain <github@sendmiles.email>
2021-04-24 17:06:32 +0000

    Improve service page when no hits are recorded

--- shynet/dashboard/views.py
+++ shynet/dashboard/views.py
@@ -66,6 +66,7 @@ class ServiceView(
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
+        data["script_protocol"] = "https://" if settings.SCRIPT_USE_HTTPS else "http://"
         data["stats"] = self.object.get_core_stats(data["start_date"], data["end_date"])
         data["object_list"] = Session.objects.filter(
             service=self.get_object(),
