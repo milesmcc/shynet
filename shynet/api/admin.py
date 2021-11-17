66b841fd86a398e3a738ece90d74ea6f5b11f83a
Paweł Jastrzębski <pj@napcode.eu>
2021-11-17 11:00:52 +0100

    Move token to User model + add API setting view

--- shynet/api/admin.py
+++ shynet/api/admin.py
@@ -1,9 +1,1 @@
-from django.contrib import admin
-from api.models import ApiToken
-
-
-class ApiTokenAdmin(admin.ModelAdmin):
-    list_display = ("name", "user", "value")
-
-
-admin.site.register(ApiToken, ApiTokenAdmin)
+# from django.contrib import admin
