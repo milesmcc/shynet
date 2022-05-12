d9bbeea89278b814e6b5faad14a3d9102e2686bb
Paweł Jastrzębski <pj@napcode.eu>
2022-05-12 12:10:44 +0200

    Remove basic option from API
    For simplicity

--- shynet/core/models.py
+++ shynet/core/models.py
@@ -113,21 +113,21 @@ class Service(models.Model):
             start_time=timezone.now() - timezone.timedelta(days=1)
         )
 
-    def get_core_stats(self, start_time=None, end_time=None, basic=False):
+    def get_core_stats(self, start_time=None, end_time=None):
         if start_time is None:
             start_time = timezone.now() - timezone.timedelta(days=30)
         if end_time is None:
             end_time = timezone.now()
 
-        main_data = self.get_relative_stats(start_time, end_time, basic)
+        main_data = self.get_relative_stats(start_time, end_time)
         comparison_data = self.get_relative_stats(
-            start_time - (end_time - start_time), start_time, basic
+            start_time - (end_time - start_time), start_time
         )
         main_data["compare"] = comparison_data
 
         return main_data
 
-    def get_relative_stats(self, start_time, end_time, basic=False):
+    def get_relative_stats(self, start_time, end_time):
         Session = apps.get_model("analytics", "Session")
         Hit = apps.get_model("analytics", "Hit")
 
@@ -151,28 +151,6 @@ class Service(models.Model):
 
         bounces = sessions.filter(is_bounce=True)
         bounce_count = bounces.count()
-
-        avg_load_time = hits.aggregate(load_time__avg=models.Avg("load_time"))[
-            "load_time__avg"
-        ]
-
-        avg_hits_per_session = hit_count / session_count if session_count > 0 else None
-
-        avg_session_duration = self._get_avg_session_duration(sessions, session_count)
-        if basic:
-            return {
-                "currently_online": currently_online,
-                "session_count": session_count,
-                "hit_count": hit_count,
-                "has_hits": has_hits,
-                "bounce_rate_pct": bounce_count * 100 / session_count
-                if session_count > 0
-                else None,
-                "avg_session_duration": avg_session_duration,
-                "avg_load_time": avg_load_time,
-                "avg_hits_per_session": avg_hits_per_session,
-                "online": True,
-            }
 
         locations = (
             hits.values("location")
@@ -219,6 +197,14 @@ class Service(models.Model):
             .annotate(count=models.Count("device"))
             .order_by("-count")
         )
+
+        avg_load_time = hits.aggregate(load_time__avg=models.Avg("load_time"))[
+            "load_time__avg"
+        ]
+
+        avg_hits_per_session = hit_count / session_count if session_count > 0 else None
+
+        avg_session_duration = self._get_avg_session_duration(sessions, session_count)
 
         chart_data, chart_tooltip_format, chart_granularity = self._get_chart_data(
             sessions, hits, start_time, end_time, tz_now
