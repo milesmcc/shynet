4d7c036acc7d1730d73323fc05306bf45d4321a2
Matt Ronchetto <hello@doamatto.xyz>
2022-08-27 18:35:56 -0700

    Add support for GPC header (#219)
    * chore: reflect both headers in debugging
    
    * chore: add Sec-GPC handling with DNT handling
    
    The `if` statement is there purely so that nothing more has to change handling wise. If either value is true, DNT policy should kick in and no data should be stored/tracked. *Should* just work™.
    
    * fix: meet Black style guide
    
    * fix: comply with other header formatting
    
    * fix: header typo

--- shynet/analytics/views/ingress.py
+++ shynet/analytics/views/ingress.py
@@ -29,6 +29,9 @@ def ingress(request, service_uuid, identifier, tracker, payload):
     location = request.META.get("HTTP_REFERER", "").strip()
     user_agent = request.META.get("HTTP_USER_AGENT", "").strip()
     dnt = request.META.get("HTTP_DNT", "0").strip() == "1"
+    gpc = request.META.get("HTTP_SEC_GPC", "0").strip() == "1"
+    if gpc or dnt:
+        dnt = True
 
     ingress_request.delay(
         service_uuid,
