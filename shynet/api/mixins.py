6d92babe20a3f5232883f5cf03098a0b67703068
Sumit Singh <sumit.singh4613@gmail.com>
2022-09-26 17:50:57 +0530

    refactor(api): sort imports and ran black on api app

--- shynet/api/mixins.py
+++ shynet/api/mixins.py
@@ -1,23 +1,27 @@
-from django.http import JsonResponse
-from django.contrib.auth.models import AnonymousUser
+from http import HTTPStatus
 
-from core.models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
+from django.http import JsonResponse
+
+User = get_user_model()
 
 
 class ApiTokenRequiredMixin:
     def _get_user_by_token(self, request):
-        token = request.headers.get('Authorization')
-        if not token or not token.startswith('Token '):
+        token = request.headers.get("Authorization")
+        if not token or not token.startswith("Token "):
             return AnonymousUser()
 
-        token = token.split(' ')[1]
-        user = User.objects.filter(api_token=token).first()
+        token = token.split(" ")[1]
+        user: User = User.objects.filter(api_token=token).first()
 
-        return user if user else AnonymousUser()
+        return user or AnonymousUser()
 
     def dispatch(self, request, *args, **kwargs):
         request.user = self._get_user_by_token(request)
-        if not request.user.is_authenticated:
-            return JsonResponse(data={}, status=403)
-
-        return super().dispatch(request, *args, **kwargs)
+        return (
+            super().dispatch(request, *args, **kwargs)
+            if request.user.is_authenticated
+            else JsonResponse(data={}, status=HTTPStatus.FORBIDDEN)
+        )
